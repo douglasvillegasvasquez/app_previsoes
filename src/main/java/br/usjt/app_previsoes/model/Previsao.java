18456ede020c6c3becf487a6f7a23acd50a440b0
[douglasvillegasvasquez] <douglas.villegas.vasquez1@gmail.com>
2020-05-13 00:13:51 -0300

    Adicao atividade 2

---
+++ src/main/java/br/usjt/app_previsoes/model/Previsao.java
@@ -0,0 +1,118 @@
+package br.usjt.app_previsoes.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Previsao implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="DIASEMANA")
+	private String diaSemana;
+	
+	@Column(name="TEMPMIN")
+	private Double tempMin;
+	
+	@Column(name="TEMPMAX")
+	private Double tempMax;
+	
+	@Column(name="UMIDADE")
+	private int umidade;
+	
+	@Column(name="DESCRICAO")
+	private String descricao;
+	
+	@Column(name="DATAHORA")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataHora;
+	
+	@Column(name="LATITUDE")
+	private Double latitude;
+	
+	@Column(name="LONGITUDE")
+	private Double longitude;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDiaSemana() {
+		return diaSemana;
+	}
+
+	public void setDiaSemana(String diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
+	public Double getTempMin() {
+		return tempMin;
+	}
+
+	public void setTempMin(Double tempMin) {
+		this.tempMin = tempMin;
+	}
+
+	public Double getTempMax() {
+		return tempMax;
+	}
+
+	public void setTempMax(Double tempMax) {
+		this.tempMax = tempMax;
+	}
+
+	public int getUmidade() {
+		return umidade;
+	}
+
+	public void setUmidade(int umidade) {
+		this.umidade = umidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(Date dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+}
